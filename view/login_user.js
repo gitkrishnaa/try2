a5c17d3b9c8386a8d80022941bd5dc339cfbaaa3
krishna kumar <kri821084@gmail.com>
2023-05-01 23:07:39 +0530

    login page in view fronted side created

---
+++ view/login_user.js
@@ -0,0 +1,43 @@
+// const x=axios.post('http://localhost:4200/user/signupApi', {
+//     firstName: 'Fred',
+//     lastName: 'Flintstone'
+//   })
+// //   .then(function (response) {
+// //     console.log(response);
+// //   })
+// //   .catch(function (error) {
+// //     console.log(error);
+// //   });
+// x.then(a=>{console.log(a,"yghg")})
+// const user_name=document.getElementById("user_name")
+// const user_email=document.getElementById("user_email")
+const user_mobile=document.getElementById("user_mobile")
+const user_password=document.getElementById("user_password")
+const sign_up_btn=document.getElementById("login_btn")
+
+
+
+sign_up_btn.addEventListener("click",()=>{
+
+  async function abc(){
+    //this function return respusnse as promise which acess using .then(), .catch()
+    return await axios.post('http://localhost:4200/user/signup', {
+      
+       email:user_email.value,
+      
+       password:user_password.value
+      })
+      
+}
+abc().then(a=>{
+  console.log(a,"ok")
+  alert("sign up sucessfull")
+})
+.catch(err=>{console.log(err)
+alert("user already exist")
+})
+
+})
+
+
+
