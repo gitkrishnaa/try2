9c9dbfc7dd10dfd913eeee4674eeb6624fb63a2d
krishna kumar <kri821084@gmail.com>
2023-05-29 09:11:31 +0530

    ok

---
+++ notesAnd_tryCode/codeTry.js
@@ -0,0 +1,16 @@
+// require('dotenv').config();
+
+
+// if (!process.env.RAZORPAY_KEY) {
+//     throw new Error('razor pay key not exist');
+//     return;
+//   }
+//   console.log(process.env.RAZORPAY_KEY);
+
+
+// checking is_envdata avilable
+const fun=require("../custom/taskFunction").is_env_data_aviliable
+fun(1,2,5,4)
+         //working fine
+
+
